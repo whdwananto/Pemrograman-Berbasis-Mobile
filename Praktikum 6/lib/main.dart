43ec49882f0fb1ff0a4cd8226088449f221dd3f4
whdwananto <whdwananto@gmail.com>
2022-11-21 08:50:12 +0700

    commit

---
+++ Praktikum 6/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Future<SharedPreferences> _preference = SharedPreferences.getInstance();
+  int _counter = 0;
+
+  Future<void> _incrementCounter() async {
+    final SharedPreferences prefs = await _preference;
+    setState(() {
+      _counter++;
+    });
+    prefs.setInt('counter', _counter);
+  }
+
+  _getCounter() async {
+    final SharedPreferences prefs = await _preference;
+    _counter = prefs.getInt('counter') ?? _counter;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCounter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
